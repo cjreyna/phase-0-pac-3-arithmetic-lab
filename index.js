b2dab89fea4527bc5b6dc903a00699853e0264db
Charles <cjreyna5150@gmail.com>
2022-03-21 19:55:33 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+	return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+let number = 10
+function increment(n) {
+    return n + 1
+}
+increment
+function add5() {
+    return (number += 5);
+}
+function divideBy3 () {
+    return (number /= 3);
+}
+;
+function decrement(n) {
+    return n - 1
+}
+function makeInt(n) {
+ return(parseInt(n, 10))
+}
+function preserveDecimal(n) {
+    return(parseFloat(n))
+}
